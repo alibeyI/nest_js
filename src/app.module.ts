0c082e28bd7d1e5227ac0ba6afc6020499cb9682
alibay <alibay@pop-os.localdomain>
2021-06-14 20:09:13 +0400

    first

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import { ItemsModule } from './items/items.module';
+import  config  from './config/keys';
+
+import * as mongoose from 'mongoose';
+
+
+
+@Module({
+  imports: [
+    ItemsModule,
+    MongooseModule.forRoot(config.mongoURI)
+  ],
+  controllers: [AppController ],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,9 @@
+export default {
+  mongoURI:'mongodb+srv://ali:8ubrwQBsHCUBoOhq@cluster0.thh6d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  useFindAndmodify:false,
+  findOneAndDelete:false,
+  findOneAndUpdate:false,
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  
+}
